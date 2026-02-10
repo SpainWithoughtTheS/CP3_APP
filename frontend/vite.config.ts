c020dd1881bb15c9e9741d441460c951da843ca5
SpainWithoughtTheS <103275152+SpainWithoughtTheS@users.noreply.github.com>
2026-02-09 20:46:00 -0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/CP3_APP/', // ⭐ your repo name EXACTLY
   plugins: [react()],
   server: {
     port: 5173
   }
-});
+})
